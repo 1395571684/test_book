83f9c3a05b87ae5315ccb13a1228208d660f13e8
1395571684 <18363990115@163.com>
2016-04-28 19:23:39 +0800

    7.5 单例模式示范（加注释）

--- src/testseven/Student.java
+++ src/testseven/Student.java
@@ -3,11 +3,13 @@ package testseven;
 
 public class Student {
 	String stuName;
-	static Student stu=new Student("王云");
+	static Student stu=new Student("王云");//static保证只能有一个实例
+	//构造方法私有化
 	private Student (String name) {
 		stuName=name;
 		System.out.println("学生的姓名为"+stuName);
 			}
+	//静态公有方法返回类对象
 	public static Student get(){
 		return stu;
 	}
